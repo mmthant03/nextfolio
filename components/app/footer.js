16ede9106123f4a5a0338d4cef2bd9bffd1fb122
Myo Min Thant <myominthant@mm.inyaland.com>
2021-07-02 00:23:24 +0630

    Removed typos from footer

--- components/app/footer.js
+++ components/app/footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="bg-black mt-16 pt-8 pb-16">
             <div className="md:max-w-screen-sm lg:max-w-screen-xl mx-auto px-4 flex items-center">
-                <div className="w-2/3 text-white text-xs">
+                <div className="w-2/3 hidden text-white text-xs">
                     <b>
                         &copy; {new Date().getFullYear()} Myo Min Thant.
                     </b>{" "}
